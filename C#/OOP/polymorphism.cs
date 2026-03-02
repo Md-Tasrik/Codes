d0dd31e8bd07ccbbfd849d7b94b25316592a464f
MD. TASRIK <118077308+Md-Tasrik@users.noreply.github.com>
2026-03-03 00:09:13 +0600

    Add method overloading for addition in Calculator
    Implement method overloading in Calculator class.

---
+++ C#/OOP/polymorphism.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Calculator {
+    public int Add(int a, int b) {
+        return a + b;
+    }
+
+    public double Add(double a, double b) {
+        return a + b;
+    }
+}
+
+class Program {
+    static void Main() {
+        Calculator calc = new Calculator();
+        Console.WriteLine(calc.Add(5, 10));     // Calls int version
+        Console.WriteLine(calc.Add(2.5, 3.5));  // Calls double version
+    }
+}
